543eab881fbf425406d45b20d835e4bec65799ab
Callen Schultz <callen.schultz@gmail.com>
2021-10-01 11:03:55 -0500

    Completed parts one and two

--- about.js
+++ about.js
@@ -5,10 +5,20 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('The form has been submitted!');
 }
 
+function compliment(evt){
+	evt.preventDefault();
+	let complimentList = document.querySelector("#compliment-list");
+	let compliment=complimentList.querySelectorAll('li');
+	let randomCompliment=compliment[Math.floor(Math.random()*3)].textContent;
+	alert(`${randomCompliment}`)
+}
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+let ducky = document.querySelector('#ducky-img');
+ducky.addEventListener('mouseenter',compliment )
